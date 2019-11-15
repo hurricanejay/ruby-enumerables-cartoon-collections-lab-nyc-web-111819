801763c2920384ed09ba8ba75e982b48bf34cf6d
Jay Wen <jaywen89@gmail.com>
2019-11-15 18:15:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,14 @@
 def roll_call_dwarves# code an argument here
   # Your code here
-  puts 
+dwarves = %w[
+  Doc
+  Dopey
+  Bashful
+  Grumpy
+  Sneezy
+  Sleepy
+  Happy
+]
 end
 
 def summon_captain_planet# code an argument here
